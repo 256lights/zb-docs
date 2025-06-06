fa90520e9596b81c8a3e1119732a3ccea882f455
Roxy Light <roxy@256lights.llc>
2025-06-06 11:13:12 -0700

    sphinx-quickstart

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        python = pkgs.python3.withPackages (ps: [
+          ps.myst-parser
+          ps.sphinx
+        ]);
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            python
+          ];
+        };
+      }
+    );
+}
