74372a180c1b938293a853bac94a05459e46c870
Roxy Light <roxy@256lights.llc>
2025-07-22 09:42:06 -0700

    Ignore README in build

--- conf.py
+++ conf.py
@@ -34,7 +34,7 @@ myst_enable_extensions = [
 ]
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.md']
 primary_domain = 'lua'
 
 # -- Options for HTML output -------------------------------------------------
